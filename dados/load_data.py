1e37e0bd3e58c77a71127076cabf5f709fe6d86c
luangonzalez2004-eng <luan.gonzalez2004@gmail.com>
2026-05-16 23:10:44 -0300

    Update load_data.py

--- dados/load_data.py
+++ dados/load_data.py
@@ -1,14 +1,15 @@
-import pandas as pd
+import panda as pd
 import sqlite3
 
-# 1. Carregar os dados
-df = pd.read_csv('data/raw/stores_sales_forecasting.csv')
+# 1 -carregar os dados 
+df = pd.read_csv('dados/stores_sales_forecasting.csv')
 
-# 2. Conectar ao banco (cria se não existir)
+# 2 - Conectar ao banco (cria se não existir)
 conn = sqlite3.connect('database/vendas.db')
 
-# 3. Salvar os dados na tabela
-df.to_sql('vendas_forecast', conn, if_exists='replace', index=False)
+# 3 - Salvar ps dadps na tabela 
+df.to_sql('vendas_forescast', conn, if_exists='replace', index=False)
 
-print("Dados carregados com sucesso!")
-conn.close()
+print("Dados carregados com sucesso!)
+      conn. close()
+      
