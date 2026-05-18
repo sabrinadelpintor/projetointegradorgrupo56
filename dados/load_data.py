2dc6dd75c44254427bf07beb9e454e0fb53ee239
Ana luiza <nalusantos3@gmail.com>
2026-05-17 21:15:44 -0300

    Fix data loading and database connection logic
    Corrigindo erros de sintaxe e caminhos no load_data.py

--- dados/load_data.py
+++ dados/load_data.py
@@ -1,15 +1,18 @@
-import panda as pd
+import pandas as pd
 import sqlite3
 
-# 1 -carregar os dados 
-df = pd.read_csv('dados/stores_sales_forecasting.csv')
+# 1 - carregar os dados 
+df = pd.read_csv('stores_sales_forecasting.csv', encoding='latin1')
 
-# 2 - Conectar ao banco (cria se não existir)
-conn = sqlite3.connect('database/vendas.db')
+# 2 - Conectando ao banco de dados SQLite (vai criar o arquivo vendas.db na mesma pasta)
+conn = sqlite3.connect('vendas.db')
 
-# 3 - Salvar ps dadps na tabela 
-df.to_sql('vendas_forescast', conn, if_exists='replace', index=False)
+# 3 - Salvando os dados no banco na tabela correta 
+df.to_sql('vendas_forecast', conn, if_exists='replace', index=False)
 
-print("Dados carregados com sucesso!)
-      conn. close()
+# Mensagem de sucesso corrigida
+print("Dados carregados com sucesso!")
+
+# Fechando a conexão corretamente
+conn.close()
       
